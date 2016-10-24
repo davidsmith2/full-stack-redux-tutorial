b7c5c747af1b58b51315ff23f4d79c506e89f7b8
David Smith <davidsmith2@gmail.com>
2016-10-24 17:53:49 -0400

    Getting Data In from Redux to React

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 import App from './components/App';
-import Voting from './components/Voting';
-import Results from './components/Results';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
 import reducer from './reducer';
 
 const store = createStore(reducer);
@@ -20,11 +21,13 @@ store.dispatch({
 });
 
 const routes = <Route component={App}>
-	<Route path="/" component={Voting} />
-	<Route path="/results" component={Results} />
+	<Route path="/results" component={ResultsContainer} />
+	<Route path="/" component={VotingContainer} />
 </Route>;
 
 ReactDOM.render(
-	<Router history={hashHistory}>{routes}</Router>,
+	<Provider store={store}>
+		<Router history={hashHistory}>{routes}</Router>
+	</Provider>,
 	document.getElementById('app')
 );
